6de7679ad4c157bcac11d3f07bd6dc472d3bd6ab
Firebird492 <gavinbuier@outlook.com>
2025-03-21 16:54:11 -0400

    ignore tests for components which might not be rendered

--- parser.py
+++ parser.py
@@ -12,11 +12,14 @@ class Parser:
         self.imageIds = []
         self.textPlaceholders = []
         self.functionName = ""
+        self.needExpoFontMock = False
 
     
     def parseFile(self):
         with open(self.filename, 'r') as file:
             bracketCount = 0
+            openBrackets = 0
+            lookForOpenBracket = False
             currentLine = ""
             for line in file:
                 # get the function name
@@ -26,56 +29,76 @@ class Parser:
                     startLine = line.find("export default function ")
                     endline = line.find("(")
                     self.functionName = line[startLine + len("export default function "):endline]
+                    
+
+                if "const" in line and "=>" in line:
+                    lookForOpenBracket = True
+
+                if "from 'react-native-vector-icons" in line:
+                    self.needExpoFontMock = True
+
+                if lookForOpenBracket or openBrackets != 0:
+                    openBrackets += line.count("{")
+                    openBrackets -=  line.count("}")
+
+                    if openBrackets != 0:
+                        lookForOpenBracket = False
+                    if openBrackets < 0:
+                        print("set 0")
+                        openBrackets = 0
 
                 if self.inFunction:
+                    #check if the function is closed
+                    bracketCount += line.count('{')
+                    bracketCount -= line.count('}')
+                    if bracketCount == 0:
+                        self.inFunction = False
+
                     # get the text show on the screen
-                    match =  re.search("<ThemedText[^>]*>", line)
-                    if match:
-                        self.openTextBlock = True
-                    if self.openTextBlock:
-                        endLine = line.find("</ThemedText>")
+                    if openBrackets == 0 and not lookForOpenBracket:
+                        match =  re.search("<ThemedText[^>]*>", line)
                         if match:
-                            startLine = line.find(match.group(0)) + len(match.group(0))
-                            if currentLine != "" and line[startLine:endLine].strip() != "":
-                                currentLine = currentLine + "\\n"
-                            currentLine = currentLine + line[startLine:endLine].strip()
-                        else:
-                            if currentLine != "" and line[:endLine].strip()!="":
-                                currentLine = currentLine + "\\n"
-                            currentLine = currentLine + line[:endLine].strip()
+                            self.openTextBlock = True
+                        if self.openTextBlock:
+                            endLine = line.find("</ThemedText>")
+                            if match:
+                                startLine = line.find(match.group(0)) + len(match.group(0))
+                                if currentLine != "" and line[startLine:endLine].strip() != "":
+                                    currentLine = currentLine + "\\n"
+                                currentLine = currentLine + line[startLine:endLine].strip()
+                            else:
+                                if currentLine != "" and line[:endLine].strip()!="":
+                                    currentLine = currentLine + "\\n"
+                                currentLine = currentLine + line[:endLine].strip()
 
-                    if "</ThemedText>" in line:
-                        self.openTextBlock = False
-                        self.foundText.append(currentLine)
-                        currentLine = ""
-                    if "{" in line:
-                        bracketCount += 1
-                    if "}" in line:
-                        bracketCount -= 1
-                        if bracketCount == 0:
-                            self.inFunction = False
-
-                    # get images
-                    if "<Image" in line:
-                        self.openImageBlock = True
+                        if "</ThemedText>" in line:
+                            self.openTextBlock = False
+                            self.foundText.append(currentLine)
+                            currentLine = ""
                     
-                    if self.openImageBlock:
-                        match = re.search(r'testID=(.*)', line)
-                        if match:
-                            self.imageIds.append(match.group(1).strip())
 
-                        if "/>" in line:
-                            self.openImageBlock = False
+                        # get images
+                        if "<Image" in line:
+                            self.openImageBlock = True
+                        
+                        if self.openImageBlock:
+                            match = re.search(r'testID=(.*)', line)
+                            if match:
+                                self.imageIds.append(match.group(1).strip())
 
-                    if "<TextInput" in line:
-                        self.openTextInput = True
-                    if self.openTextInput:
-                        match = re.search(r'placeholder=(.*)', line)
-                        if match:
-                            self.textPlaceholders.append(match.group(1).strip())
+                            if "/>" in line:
+                                self.openImageBlock = False
 
-                        if "/>" in line:
-                            self.openTextInput = False
+                        if "<TextInput" in line:
+                            self.openTextInput = True
+                        if self.openTextInput:
+                            match = re.search(r'placeholder=(.*)', line)
+                            if match:
+                                self.textPlaceholders.append(match.group(1).strip())
+
+                            if "/>" in line:
+                                self.openTextInput = False
+
 
 
         
@@ -86,19 +109,21 @@ class Parser:
             if text != "":
                 validText.append(text)
         
-        return FunctionInfo(self.functionName, validText, self.imageIds, self.textPlaceholders, self.filename[7:])
+        return FunctionInfo(self.functionName, validText, self.imageIds, 
+                            self.textPlaceholders, self.filename[7:], self.needExpoFontMock)
 
 
 
 
 
 class FunctionInfo:
-    def __init__(self, name:str, text, imageIds, textPlaceholders, filename):
+    def __init__(self, name:str, text, imageIds, textPlaceholders, filename, needExpoFontMock):
         self.name = name
         self.text = text
         self.images = imageIds
         self.textPlaceholders = textPlaceholders
         self.filename = filename
+        self.needExpoFontMock = needExpoFontMock
 
     def getName(self):
         return self.name
@@ -114,6 +139,9 @@ class FunctionInfo:
     
     def getPlaceholders(self):
         return self.textPlaceholders
+    
+    def getExpoFontMock(self):
+        return self.needExpoFontMock
     
     def getFileName(self):
         return self.filename.split(".")[0]
